743822fe284379ffece6b7eab8e82edb308a9659
Ahmet Efe Zoralioğlu <ahmetefezorali@gmail.com>
2022-08-08 11:19:18 +0300

    7 august - dev1-whitenoise edit

--- sot/settings.py
+++ sot/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from os.path import join
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,8 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
+STATICFILES_DIR = [join(BASE_DIR, 'static')]
+MEDIA_ROOT = join(BASE_DIR, 'static/iamges')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
